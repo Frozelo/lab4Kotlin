8b7232f52b58c98396ba8768ed5b988c3ac043be
ivansizov <sizovivan.05@gmail.com>
2024-10-05 12:32:19 +0700

    small fixes

--- app/src/main/java/com/example/lab41/MainActivity.kt
+++ app/src/main/java/com/example/lab41/MainActivity.kt
@@ -17,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         R.string.question_australia,
         R.string.question_oceans,
         R.string.question_mideast,
-
+        R.string.question_africa,
+        R.string.question_americas,
+        R.string.question_asia
     )
 
 
@@ -139,7 +141,7 @@ class MainActivity : AppCompatActivity() {
         resetQuiz()
     }
 
-
+    // Сбрасываем викторину для нового прохождения
     private fun resetQuiz() {
         correctAnswers = 0
         currentIndex = 0
