52b8a927a01b7350883a1e17d395019dd0e1d9b9
ivansizov <sizovivan.05@gmail.com>
2024-11-24 16:06:23 +0700

    add comments

--- app/src/main/java/com/example/lab41/CheatActivity.kt
+++ app/src/main/java/com/example/lab41/CheatActivity.kt
@@ -7,23 +7,30 @@ import com.example.lab41.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
 
+    // инициализируем биндниг для работы CheatActivity
     private lateinit var binding: ActivityCheatBinding
 
+    // метод который вызывается при открытии activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // выводим activity
         binding = ActivityCheatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val apiLevel = Build.VERSION.SDK_INT
-        binding.apiLevelText.text = "API Level $apiLevel"
 
+
+        // задаём поведенеи кнопки по выводу подсказкки
         binding.showAnswerButton.setOnClickListener {
+            // распаковывем в переменную
             val answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+            // показываем в тексте - answerdIsTrue = True, пишем в тексте True, иначе False
             binding.answerTextView.setText(if (answerIsTrue) R.string.true_button else R.string.false_button)
         }
     }
 
+
+    // Сопудствующий объект - объект, который необходим от activity
     companion object {
         const val EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true"
     }
--- app/src/main/java/com/example/lab41/MainActivity.kt
+++ app/src/main/java/com/example/lab41/MainActivity.kt
@@ -11,8 +11,10 @@ import com.example.lab41.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // инициализируем биндниг для работы MainActivity
     private lateinit var binding: ActivityMainBinding
 
+    // Инициализируем необходимые вопрсы из res/strings
     private val questionBank = listOf(
         R.string.question_australia,
         R.string.question_oceans,
@@ -22,21 +24,26 @@ class MainActivity : AppCompatActivity() {
         R.string.question_asia
     )
 
-
+    // инициализируем список из правильный ответов
     private val answers = listOf(true, true, false, false, false, true)
 
+    // инициализируем счетчики (текущий вопрос, правильные ответы и флаг ответили/не ответили)
     private var currentIndex = 0
     private var correctAnswers = 0
     private var answered = false
 
+
+    // метод который вызывается при открытии activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
+        // вызываем наше activity
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
+        // сохраняем состояние при поворете устройсва
         if (savedInstanceState != null) {
             currentIndex = savedInstanceState.getInt("CURRENT_INDEX", 0)
             correctAnswers = savedInstanceState.getInt("CORRECT_ANSWERS", 0)
@@ -44,50 +51,68 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        //точка входа в наш quiz - отображаем вопрос с индексом 1
         updateQuestion()
 
 
+        // задаём поведение кнопки true
         binding.trueButton.setOnClickListener {
             if (!answered) {
                 checkAnswer(true)
                 answered = true
+                // скрываем кнпоку
                 hideButtons()
             }
         }
 
-
+    // задаём поведение кнопки false
         binding.falseButton.setOnClickListener {
             if (!answered) {
                 checkAnswer(false)
                 answered = true
+                // скрываем кнпоку
                 hideButtons()
             }
         }
 
-
+        // задаём поведение кнопки next
         binding.nextButton.setOnClickListener {
+            // функциональная проверка на завершение воросов
+            //  проверка, что мы прошли все вопросы
             if (currentIndex == questionBank.size - 1) {
 
+                // показывает результат
                 showResults()
             } else {
 
-                currentIndex = (currentIndex + 1) % questionBank.size
+                // прибавляем к текущему индексу
+                currentIndex = (currentIndex + 1)
                 answered = false
+
+                // обновляем строку
                 updateQuestion()
             }
         }
 
 
+        // задаём поведение кнопки cheat
         binding.cheatButton.setOnClickListener {
+            // достаём правильный ответ на текущий вопрос и сохраняем в переменную
             val answerIsTrue = answers[currentIndex]
+
+            // intent - механизм для создания нового активити окна - создаём и передаём необходимые параметры
+            // (answerdIsTrue)
             val intent = Intent(this, CheatActivity::class.java).apply {
                 putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue)
             }
+
+            // запускаем activity с переданными объетктом класса Intent
             startActivity(intent)
         }
     }
 
 
+    // Функция для сохранения состояния
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putInt("CURRENT_INDEX", currentIndex)
@@ -96,15 +121,19 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    // берет из списка вопросов очередной вопрос и отображает его компоненты, храня на него ответ
     private fun updateQuestion() {
+        // берет индекс и ставит текст вопроса
         val questionResId = questionBank[currentIndex]
         binding.questionText.setText(questionResId)
 
 
+        // ставит кнопки true/false видимыми
         binding.trueButton.visibility = Button.VISIBLE
         binding.falseButton.visibility = Button.VISIBLE
 
 
+        // проверка того, что мы прошли все вопросы - меняем текст в кнопке
         if (currentIndex == questionBank.size - 1) {
             binding.nextButton.text = "Finish"
         } else {
@@ -113,39 +142,53 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    // функция, которая ставит кнопки в положение "invisible"
     private fun hideButtons() {
         binding.trueButton.visibility = Button.INVISIBLE
         binding.falseButton.visibility = Button.INVISIBLE
     }
 
 
+    // функция, которая проверяет ответ
     private fun checkAnswer(userAnswer: Boolean) {
+        // достаёт ответ из списка ответов по индексу
         val correctAnswer = answers[currentIndex]
+        // сравнивает ответ пользователя с ответом в списке ответов
         if (userAnswer == correctAnswer) {
+
+            // если всё ок, прибавляем к счётчику
             correctAnswers++
+            // используем Toast, отображаем, что мы ответили правильно
             Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
         } else {
+            // отображаем, что мы ответили неправильно
             Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
     }
 
 
+    // используем компонент AlertDialog для отображения ответа в виде системного окошка
     private fun showResults() {
         AlertDialog.Builder(this)
+            // форматируем строки под ответ
             .setTitle("Результат")
             .setMessage("Вы ответили правильно на $correctAnswers из ${questionBank.size} вопросов!")
             .setPositiveButton("OK") { _, _ -> }
             .show()
 
 
+        // сбрасываем состояние для тестирования
         resetQuiz()
     }
 
-    // Сбрасываем викторину для нового прохождения
+
+    // всё зануляем
     private fun resetQuiz() {
         correctAnswers = 0
         currentIndex = 0
         answered = false
+
+        // начинаем тест заново
         updateQuestion()
     }
 }
